9688b188b303ca5ba0104bcba4664df88645427f
Pujan Srivastava <psrivastava@petsafe.net>
2020-04-28 11:27:47 +0100

    Init

--- routes/healthCheckRoutes.ts
+++ routes/healthCheckRoutes.ts
@@ -1,23 +1,8 @@
 class HealthCheckRoutes {
     public static handler = async (event: any, context: any) => {
 
-        let response: any;
-        switch (event.httpMethod) {                                                                                 // Routing
-            case "GET":                                                                                             // Handle GET method
-                switch (event.path) {
-                    case "/health-check":
-                        response = {
-                            message: 'Health Check for Product Service',
-                            event
-                        };
-                        break;
-                    default:
-                        throw new Error('Unknown path');
-                }
-                break;
-            default:
-                throw new Error('Unknown path');
-        }
+        const response: any = { message: 'Health Check for Product Service', event};
+                        
         return {
             statusCode: 200,
             body: JSON.stringify(response)
